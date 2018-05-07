9fddb25ccb3ee6288c8fdcfdd44bc27130867e6e
Patrick Mokesch <mokesch.pat@gmail.com>
2018-05-07 21:22:44 +0200

    first concept plentyplugin
    Installing first Setup for Plentymarkets Plugin (App) - Start Settings - tutorial from Plenty

---
+++ src/Providers/pmopluginServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace pmoplugin\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+class pmopluginServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+}
